290414d9940f90fdc5753aa363e9f3b12b0d14c5
Manoj Pawar <mmpawar94@gmail.com>
2024-11-01 10:17:13 +0530

    added style to components

--- src/component/header.jsx
+++ src/component/header.jsx
@@ -4,16 +4,16 @@ import React from "react";
 const Header = () => (
   <>
     <div className="row" style={{ textAlign: "center" }}>
-      <div className="col-xs-6 col-sm-6 col-md-3">
+      <div className="col-6 col-sm-6 col-md-3">
         <Typography color="primary">||ॐ श्री गणेशाय नमः||</Typography>
       </div>
-      <div className="col-xs-6 col-sm-6 col-md-3">
+      <div className="col-6 col-sm-6 col-md-3">
         <Typography color="primary">||ॐ श्री साईनाथय नमः||</Typography>
       </div>
-      <div className="col-xs-6 col-sm-6 col-md-3">
+      <div className="col-6 col-sm-6 col-md-3">
         <Typography color="primary">||ॐ श्री नित्यांदाय नमः||</Typography>
       </div>
-      <div className="col-xs-6 col-sm-6 col-md-3">
+      <div className="col-6 col-sm-6 col-md-3">
         <Typography color="primary">||ॐ श्री सदानंदाय नमः||</Typography>
       </div>
     </div>
